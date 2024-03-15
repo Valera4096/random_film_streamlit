e2b27360124b943ec4dc2fc88c7b0978d7a3f220
Valera <Fyodorov.val99@gmail.com>
2024-03-15 11:11:50 +0300

    Перерароботка работы фильтров и многое другое

--- stream.py
+++ stream.py
@@ -9,9 +9,11 @@ from io import BytesIO
 from sentence_transformers import SentenceTransformer
 import faiss
 
+import pickle
+
 st.markdown(f'<p style="background-color: #262730; color: white ; font-size: 40px; font-weight: bold; text-align:">Умный поиск фильмов</p>', unsafe_allow_html=True)
 
-df = pd.read_csv("movies.csv")
+df = pd.read_csv("/resources/movies.csv")
 
 @st.cache_resource
 def load_model():
@@ -20,8 +22,16 @@ model = load_model()
         
 @st.cache_resource
 def load_embeddings():
-    return np.load('embeding.npy')
+    return np.load('/resources/embeding.npy')
 embeddings = load_embeddings()
+
+@st.cache_resource
+def load_lst():    
+    with open('/resources/lst_actor.pkl', 'rb') as f:
+        lst_actor_loaded = pickle.load(f)
+    return lst_actor_loaded
+
+lst_actor_loaded = load_lst()
 
 
 index = faiss.IndexFlatIP(embeddings.shape[1])
@@ -42,6 +52,7 @@ def display_movie(i):
     descrip_movie = df['description'][i]
     imges = df['img_url'][i]
     genre_movie = "Нет данных" if pd.isna(df['genres'].iloc[i]) else df['genres'][i]
+    actor_movie = df['actors'][i]
     imb = 'Нет оценки' if df['imdb'][i] == 0 else str(df['imdb'][i])
     end = '_'*37
     write_movie = f'''
@@ -56,6 +67,7 @@ def display_movie(i):
             <p style="font-size: 20px; font-weight: bold;">Описание:</p>
             <p style="font-size: 15px; font-weight: bold;">{descrip_movie}</p>
             <p style="font-size: 20px; font-weight: bold;">Жанр: {genre_movie}</p>
+            <p style="font-size: 15px; font-weight: bold;">Актерский состав: {actor_movie}</p>
             <p style="font-size: 20px; font-weight: bold;">Оценка: {imb}</p>
             <p style="font-size: 20px; font-weight: bold;">{end}</p>
         </div>
@@ -65,7 +77,7 @@ def display_movie(i):
 
 
 # Загрузить изображение из файла
-img = Image.open('_-fotor.jpg')
+img = Image.open('/resources/_-fotor.jpg')
 buffered = BytesIO()
 img.save(buffered, format="JPEG")
 img_str = base64.b64encode(buffered.getvalue()).decode()
@@ -83,44 +95,62 @@ st.markdown(f"""
 
 #Фильтры
 if st.sidebar.toggle('Фильтр'):
-    options = st.sidebar.multiselect(
-        'Выберите жанры',
-        ['триллер','боевик','драма','комедия','мелодрама','комедия','детектив','криминал','боевик',
-         'фантастика','фэнтези','детский','семейный','фантастика', 
-            'короткометражный','детектив','мелодрама','драма','триллер','мультфильмы',
-            'приключения','короткометражный,','мультфильмы','приключения',
-            'семейный','документальный','ужасы','биография','криминал,','документальный','исторический',
-            'аниме','вестерн','ужасы','спорт',
-            'музыкальный','военный','мюзикл','исторический','биография','вестерн','мюзикл',
-            'военный','музыкальный','детский','эротика','фэнтези','аниме'])
-    target_set = set(options)
-
-# фильтруем DataFrame
-    if len(options) != 0:
-            
-        df = df[df['genres'].apply(lambda x: target_set.issubset(x.split(', ')))].reset_index(drop= True)
-        if len(df) == 0:
-            st.title('По заданым параметрам ничего не найдено ((')
-        else:
-            filterd_embeddings = embeddings[np.array(df.index)]  
-            # создаем индекс Faiss
-            index = faiss.IndexFlatIP(filterd_embeddings.shape[1])
-            # добавляем вектора вложений в индекс Faiss
-            index.add(filterd_embeddings)
-                   
+    
     st.sidebar.header('Выбор дат')        
     year_min = st.sidebar.slider("Выбор минимального года:", min_value=1937, max_value=2022, value=2015)
     year_max = st.sidebar.slider("Выбор максимального года:", min_value=year_min, max_value=2024, value=2020)
     
-    df = df[(df['year'] >= year_min) & (df['year'] <= year_max)].reset_index(drop= True)
-    if len(df) == 0:
-        st.title('По заданым параметрам ничего не найдено ((')
-    else:
-        filterd_embeddings = embeddings[np.array(df.index)]  
-        # создаем индекс Faiss
-        index = faiss.IndexFlatIP(filterd_embeddings.shape[1])
-        # добавляем вектора вложений в индекс Faiss
-        index.add(filterd_embeddings)
+    options = st.sidebar.multiselect(
+    'Выберите жанры',
+    ['триллер','боевик','драма','комедия','мелодрама','комедия','детектив','криминал','боевик',
+        'фантастика','фэнтези','детский','семейный','фантастика', 
+        'короткометражный','детектив','мелодрама','драма','триллер','мультфильмы',
+        'приключения','короткометражный,','мультфильмы','приключения',
+        'семейный','документальный','ужасы','биография','криминал,','документальный','исторический',
+        'аниме','вестерн','ужасы','спорт',
+        'музыкальный','военный','мюзикл','исторический','биография','вестерн','мюзикл',
+        'военный','музыкальный','детский','эротика','фэнтези','аниме'])
+    target_set = set(options)
+    
+
+        
+    actors = st.sidebar.multiselect('Актерский состав',lst_actor_loaded)
+
+    actors_set = set(actors)
+
+    if st.sidebar.toggle('Применить фильтры'):
+        df = df[(df['year'] >= year_min) & (df['year'] <= year_max)].reset_index(drop= True)
+        if len(df) == 0:
+            st.title('По заданым параметрам ничего не найдено ((')
+        else:
+            filterd_embeddings = embeddings[np.array(df.index)]  
+            index = faiss.IndexFlatIP(filterd_embeddings.shape[1])
+            index.add(filterd_embeddings)    
+            
+        if len(options) != 0:
+                
+            df = df[df['genres'].apply(lambda x: target_set.issubset(x.split(', ')))].reset_index(drop= True)
+            if len(df) == 0:
+                st.title('По заданым параметрам ничего не найдено ((')
+            else:
+                filterd_embeddings = embeddings[np.array(df.index)]  
+                # создаем индекс Faiss
+                index = faiss.IndexFlatIP(filterd_embeddings.shape[1])
+                # добавляем вектора вложений в индекс Faiss
+                index.add(filterd_embeddings)
+        
+        if len(actors_set) != 0:
+            df = df[df['actors'].apply(lambda x: actors_set.issubset(x.split(', ')))].reset_index()
+            if len(df) == 0:
+                st.title('По заданым параметрам ничего не найдено ((')
+            else:
+                filterd_embeddings = embeddings[np.array(df.index)]  
+                index = faiss.IndexFlatIP(filterd_embeddings.shape[1])
+                index.add(filterd_embeddings)        
+        
+        
+        
+
 
 if len(df) !=0 :
     # Создать текстовое поле для ввода названия фильма
